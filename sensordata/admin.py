8539d421eec6322d483963c6e9b2e3f4c7cdc595
TheSwissBell <theswissbell@outlook.com>
2022-12-09 19:15:13 +0000

    tables measures and sensor types created

--- sensordata/admin.py
+++ sensordata/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Sensor)
-#admin.site.register(models.Measure)
+admin.site.register(models.SensorType)
+admin.site.register(models.Measure)
--- sensordata/models.py
+++ sensordata/models.py
@@ -2,11 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+
+class SensorType(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):		# new
+        return self.name	# new
+
 class Sensor(models.Model):
+    type = models.ForeignKey(SensorType, on_delete=models.CASCADE, blank=True)
     name = models.CharField(max_length=300)
     #measurement_date = models.DateField()
     
     def __str__(self):		# new
         return self.name	# new
 
+class Measure(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, blank=True)
+    value = models.CharField(max_length=300)
+    measurement_date = models.DateField()
+    
+    def __str__(self):		# new
+        return self.name	# new    
         
